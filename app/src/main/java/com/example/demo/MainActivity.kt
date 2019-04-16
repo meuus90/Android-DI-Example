1f3532e467a1431f9d9434188c060c027210a906
meuus90 <meuus90@bluewhale.foundation>
2019-04-16 15:32:27 +0900

    Fix EditText

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -1,14 +1,12 @@
 package com.example.demo
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-        et_test1.setHintText("test hint")
+        setContentView(R.layout.activity_main)
     }
 }
--- meuuslibs/src/main/java/com/libs/meuuslibs/widget/MeUEditText.kt
+++ meuuslibs/src/main/java/com/libs/meuuslibs/widget/MeUEditText.kt
@@ -5,6 +5,7 @@ import android.animation.ArgbEvaluator
 import android.animation.ValueAnimator
 import android.content.Context
 import android.content.res.TypedArray
+import android.graphics.Rect
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.TransitionDrawable
 import android.util.AttributeSet
@@ -16,6 +17,7 @@ import android.widget.EditText
 import android.widget.RelativeLayout
 import androidx.appcompat.content.res.AppCompatResources.getDrawable
 import androidx.core.content.ContextCompat
+import com.libs.meuuslibs.R
 import com.libs.meuuslibs.util.ConvertMetrics.Companion.dpToPx
 import kotlinx.android.synthetic.main.widget_meu_edit_text.view.*
 
@@ -34,12 +36,12 @@ class MeUEditText : ViewGroup {
 
     constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle) {
         initView(context)
-        getAttrs(attrs, defStyle)
+//        getAttrs(attrs, defStyle)
     }
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
         initView(context)
-        getAttrs(attrs)
+//        getAttrs(attrs)
     }
 
     constructor(context: Context) : super(context, null) {
@@ -48,29 +50,27 @@ class MeUEditText : ViewGroup {
 
     private fun initView(context: Context) {
         val li = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        val view = li.inflate(com.libs.meuuslibs.R.layout.widget_meu_edit_text, this, false)
+        val view = li.inflate(R.layout.widget_meu_edit_text, this, false)
         addView(view)
     }
 
     private fun getAttrs(attrs: AttributeSet) {
-        val typedArray = context.obtainStyledAttributes(attrs, com.libs.meuuslibs.R.styleable.MeUEditText)
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.MeUEditText)
         setTypeArray(typedArray)
     }
 
     private fun getAttrs(attrs: AttributeSet, defStyle: Int) {
-        val typedArray = context.obtainStyledAttributes(attrs, com.libs.meuuslibs.R.styleable.MeUEditText, defStyle, 0)
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.MeUEditText, defStyle, 0)
         setTypeArray(typedArray)
     }
 
-    private var backgroundBefore: Int = com.libs.meuuslibs.R.drawable.background_underline_black
-    private var backgroundAfter: Int = com.libs.meuuslibs.R.drawable.background_underline_black
+    private var backgroundBefore: Int = R.drawable.background_underline_black
+    private var backgroundAfter: Int = R.drawable.background_underline_black
     private var backgroundDrawablesBefore: Array<Drawable>? = null
     private var backgroundDrawablesAfter: Array<Drawable>? = null
 
-    private var hintTextColorBefore: Int = com.libs.meuuslibs.R.color.colorBlack
-    private var hintTextColorAfter: Int = com.libs.meuuslibs.R.color.colorBlack
-    private var hintTextDrawablesBefore: Array<Drawable>? = null
-    private var hintTextDrawablesAfter: Array<Drawable>? = null
+    private var hintTextColorBefore: Int = 0x000000
+    private var hintTextColorAfter: Int = 0x000000
 
     private var hintTextSizeBefore: Int = 15
     private var hintTextSizeAfter: Int = 15
@@ -81,74 +81,54 @@ class MeUEditText : ViewGroup {
     private var hintTextHorizontalPositionBefore: Int = center
     private var hintTextHorizontalPositionAfter: Int = center
 
+    private var animationDuration: Int = 1
+
     private fun setTypeArray(typedArray: TypedArray) {
-        backgroundBefore = typedArray.getResourceId(com.libs.meuuslibs.R.styleable.MeUEditText_backgroundBefore, com.libs.meuuslibs.R.drawable.background_underline_black)
-        backgroundAfter = typedArray.getResourceId(com.libs.meuuslibs.R.styleable.MeUEditText_backgroundAfter, backgroundBefore)
+        backgroundBefore = typedArray.getResourceId(R.styleable.MeUEditText_backgroundBefore, R.drawable.background_underline_black)
+        backgroundAfter = typedArray.getResourceId(R.styleable.MeUEditText_backgroundAfter, backgroundBefore)
         backgroundDrawablesBefore = arrayOf(getDrawable(context, backgroundBefore)!!, getDrawable(context, backgroundAfter)!!)
         backgroundDrawablesAfter = arrayOf(getDrawable(context, backgroundAfter)!!, getDrawable(context, backgroundBefore)!!)
         v_root.background = TransitionDrawable(backgroundDrawablesBefore)
 
         //HintText
-        val hintText = typedArray.getString(com.libs.meuuslibs.R.styleable.MeUEditText_hintText)
+        val hintText = typedArray.getString(R.styleable.MeUEditText_hintText)
         tv_hintBefore.text = hintText
         tv_hintAfter.text = hintText
+        tv_hint.text = hintText
 
-        hintTextColorBefore = typedArray.getResourceId(com.libs.meuuslibs.R.styleable.MeUEditText_hintTextColorBefore, com.libs.meuuslibs.R.color.colorBlack)
+        hintTextColorBefore = typedArray.getColor(R.styleable.MeUEditText_hintTextColorBefore, 0x000000)
         tv_hintBefore.setTextColor(ContextCompat.getColor(context, hintTextColorBefore))
-        hintTextColorAfter = typedArray.getResourceId(com.libs.meuuslibs.R.styleable.MeUEditText_hintTextColorAfter, hintTextColorBefore)
+        hintTextColorAfter = typedArray.getColor(R.styleable.MeUEditText_hintTextColorAfter, hintTextColorBefore)
         tv_hintAfter.setTextColor(ContextCompat.getColor(context, hintTextColorAfter))
         tv_hint.setTextColor(ContextCompat.getColor(context, hintTextColorBefore))
 
-        hintTextSizeBefore = typedArray.getDimensionPixelSize(com.libs.meuuslibs.R.styleable.MeUEditText_hintTextSizeBefore, dpToPx(context, 15))
+        hintTextSizeBefore = typedArray.getDimensionPixelSize(R.styleable.MeUEditText_hintTextSizeBefore, dpToPx(context, 15))
         tv_hintBefore.setTextSize(TypedValue.COMPLEX_UNIT_PX, hintTextSizeBefore.toFloat())
-        hintTextSizeAfter = typedArray.getDimensionPixelSize(com.libs.meuuslibs.R.styleable.MeUEditText_hintTextSizeAfter, hintTextSizeBefore)
+        hintTextSizeAfter = typedArray.getDimensionPixelSize(R.styleable.MeUEditText_hintTextSizeAfter, hintTextSizeBefore)
         tv_hintAfter.setTextSize(TypedValue.COMPLEX_UNIT_PX, hintTextSizeAfter.toFloat())
         tv_hint.setTextSize(TypedValue.COMPLEX_UNIT_PX, hintTextSizeBefore.toFloat())
 
-        hintTextVerticalPositionBefore = typedArray.getInt(com.libs.meuuslibs.R.styleable.MeUEditText_hintTextVerticalPositionBefore, 0)
-        hintTextVerticalPositionAfter = typedArray.getInt(com.libs.meuuslibs.R.styleable.MeUEditText_hintTextVerticalPositionBefore, hintTextVerticalPositionBefore)
+        hintTextVerticalPositionBefore = typedArray.getInt(R.styleable.MeUEditText_hintTextVerticalPositionBefore, 0)
+        hintTextVerticalPositionAfter = typedArray.getInt(R.styleable.MeUEditText_hintTextVerticalPositionBefore, hintTextVerticalPositionBefore)
         setVerticalPosition()
 
-        hintTextHorizontalPositionBefore = typedArray.getInt(com.libs.meuuslibs.R.styleable.MeUEditText_hintTextHorizontalPositionBefore, 0)
-        hintTextHorizontalPositionAfter = typedArray.getInt(com.libs.meuuslibs.R.styleable.MeUEditText_hintTextHorizontalPositionAfter, hintTextHorizontalPositionBefore)
+        hintTextHorizontalPositionBefore = typedArray.getInt(R.styleable.MeUEditText_hintTextHorizontalPositionBefore, 0)
+        hintTextHorizontalPositionAfter = typedArray.getInt(R.styleable.MeUEditText_hintTextHorizontalPositionAfter, hintTextHorizontalPositionBefore)
         setHorizontalPosition()
 
+        animationDuration = typedArray.getInt(R.styleable.MeUEditText_animationDuration, 0)
 
         //EditText
-        val editText = typedArray.getString(com.libs.meuuslibs.R.styleable.MeUEditText_editText)
+        val editText = typedArray.getString(R.styleable.MeUEditText_editText)
         et_input.setText(editText)
 
-        val editTextColor = typedArray.getResourceId(com.libs.meuuslibs.R.styleable.MeUEditText_editTextColor, com.libs.meuuslibs.R.color.colorBlack)
-        et_input.setTextColor(ContextCompat.getColor(context, editTextColor))
+        val editTextColor = typedArray.getColor(R.styleable.MeUEditText_editTextColor, 0x000000)
+        et_input.setTextColor(editTextColor)
 
-        val editTextSize = typedArray.getDimensionPixelSize(com.libs.meuuslibs.R.styleable.MeUEditText_editTextSize, dpToPx(context, 15))
+        val editTextSize = typedArray.getDimensionPixelSize(R.styleable.MeUEditText_editTextSize, dpToPx(context, 15))
         et_input.setTextSize(TypedValue.COMPLEX_UNIT_PX, editTextSize.toFloat())
 
-
-//        if (this.has()) {
-//            v_root.setBackgroundResource(backgroundBefore)
-//            tv_hint.setTextColor(ContextCompat.getColor(context, hintTextColorBefore))
-//        } else {
-//            v_root.setBackgroundResource(backgroundAfter)
-//            tv_hint.setTextColor(ContextCompat.getColor(context, hintTextColorAfter))
-//        }
-
-
         typedArray.recycle()
-    }
-
-    fun getHint(): String {
-        return tv_hintBefore.text.toString()
-    }
-
-    fun setHint(hint: String) {
-        tv_hint.text = hint
-        tv_hintBefore.text = hint
-        tv_hintAfter.text = hint
-    }
-
-    fun getEditText(): EditText {
-        return et_input
     }
 
     private fun setVerticalPosition() {
@@ -240,15 +220,21 @@ class MeUEditText : ViewGroup {
             else -> paramsHintAfter.addRule(RelativeLayout.CENTER_HORIZONTAL)
         }
         tv_hintAfter.layoutParams = paramsHintAfter
-
         tv_hint.layoutParams = paramsHintBefore
     }
 
     private fun setAnim(duration: Int) {
         val mAnimator = ValueAnimator()
         val hintParams = tv_hint.layoutParams
-        val hintWidthDifference = (tv_hintBefore.layoutParams.height - tv_hintAfter.layoutParams.height).unaryPlus()
-        val hintHeightDifference = (tv_hintBefore.layoutParams.width - tv_hintAfter.layoutParams.width).unaryPlus()
+        val hintWidthDifference = (tv_hintBefore.layoutParams.width - tv_hintAfter.layoutParams.width).unaryPlus()
+        val hintHeightDifference = (tv_hintBefore.layoutParams.height - tv_hintAfter.layoutParams.height).unaryPlus()
+
+        val smallWidth = if (tv_hintBefore.layoutParams.width < tv_hintAfter.layoutParams.width) tv_hintBefore.layoutParams.width else tv_hintAfter.layoutParams.width
+        val smallHeight = if (tv_hintBefore.layoutParams.height < tv_hintAfter.layoutParams.height) tv_hintBefore.layoutParams.height else tv_hintAfter.layoutParams.height
+
+        val hintXDifference = tv_hintAfter.x - tv_hintBefore.x
+        val hintYDifference = tv_hintAfter.y - tv_hintBefore.y
+
         if (et_input.hasFocus())
             mAnimator.setIntValues(hintTextColorBefore, hintTextColorAfter)
         else
@@ -258,23 +244,13 @@ class MeUEditText : ViewGroup {
         mAnimator.interpolator = DecelerateInterpolator()
         mAnimator.addUpdateListener {
             tv_hint.setTextColor(ContextCompat.getColor(context, it.animatedValue as Int))
-            val fraction = it.animatedFraction
+            val fraction = if (et_input.hasFocus()) it.animatedFraction else 1 - it.animatedFraction
 
-            if(et_input.hasFocus()) {
-                hintParams.width = (hintWidthDifference.toFloat() * fraction).toInt()  //todo:refactoring
-                hintParams.height = (hintHeightDifference.toFloat() * fraction).toInt()  //todo:refactoring
+            hintParams.width = (hintWidthDifference.toFloat() * fraction).toInt() + smallWidth
+            hintParams.height = (hintHeightDifference.toFloat() * fraction).toInt() + smallHeight
 
-//                tv_hint.getLocationInWindow()
-            }
-
-
-
-
-            //size
-            //position x
-            //position y
-
-
+            tv_hint.translationX = hintXDifference * fraction
+            tv_hint.translationY = hintYDifference * fraction
         }
         mAnimator.addListener(object : Animator.AnimatorListener {
             override fun onAnimationStart(animation: Animator?) {
@@ -299,7 +275,33 @@ class MeUEditText : ViewGroup {
         mAnimator.start()
     }
 
+//    private var disposable: Disposable? = null
+
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
-        setAnim(300)
+    }
+
+    override fun onFocusChanged(gainFocus: Boolean, direction: Int, previouslyFocusedRect: Rect?) {
+        super.onFocusChanged(gainFocus, direction, previouslyFocusedRect)
+//        setAnim(animationDuration)
+    }
+
+//    override fun onDetachedFromWindow() {
+//        if (disposable != null && !disposable!!.isDisposed)
+//            disposable?.dispose()
+//        super.onDetachedFromWindow()
+//    }
+
+    fun getHint(): String {
+        return tv_hintBefore.text.toString()
+    }
+
+    fun setHint(hint: String) {
+        tv_hint.text = hint
+        tv_hintBefore.text = hint
+        tv_hintAfter.text = hint
+    }
+
+    fun getEditText(): EditText {
+        return et_input
     }
 }
